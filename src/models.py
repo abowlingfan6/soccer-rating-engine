6de2770494986654624de524b4a5ff6681e87686
abowlingfan6 <asoccerfan6@gmail.com>
2026-06-20 16:20:37 -0700

    Refactor player rating functions for clarity

--- src/models.py
+++ src/models.py
@@ -1,8 +1,49 @@
+# src/models.py
+
 def defender_rating(s):
-    return 6 + s["goals"] * 0.5 - s["red_cards"]
+    return (
+        6
+        + (1.0 * s.get("Tk", 0))
+        + (0.8 * s.get("INT", 0))
+        + (0.5 * s.get("FW", 0))
+        + (0.5 * s.get("P", 0) / 50)
+        - (1.0 * s.get("RC", 0))
+        - (0.5 * s.get("YC", 0))
+        - (0.8 * s.get("GC", 0))
+        - (0.3 * s.get("O", 0))
+        + (0.5 * s.get("C", 0))
+        + (0.4 * s.get("MP", 0) / 90)
+    )
+
 
 def midfielder_rating(s):
-    return 6 + s["assists"] * 0.6
+    return (
+        6
+        + (1.2 * s.get("P", 0) / 50)
+        + (1.0 * s.get("C", 0))
+        + (0.8 * s.get("Tk", 0))
+        + (0.8 * s.get("INT", 0))
+        + (0.6 * s.get("FW", 0))
+        + (0.5 * s.get("A", 0))
+        - (0.8 * s.get("RC", 0))
+        - (0.3 * s.get("YC", 0))
+        - (0.6 * s.get("O", 0))
+        + (0.4 * s.get("MP", 0) / 90)
+    )
+
 
 def forward_rating(s):
-    return 6 + s["goals"] * 1.2 + s["shots_on_target"] * 0.3
+    return (
+        6
+        + (2.0 * s.get("G", 0))
+        + (1.2 * s.get("SOnT", 0))
+        + (0.6 * s.get("SOffT", 0))
+        + (1.0 * s.get("A", 0))
+        + (0.8 * s.get("FW", 0))
+        + (0.5 * s.get("C", 0))
+        + (0.5 * s.get("P", 0) / 50)
+        - (0.5 * s.get("O", 0))
+        - (0.4 * s.get("RC", 0))
+        - (0.2 * s.get("YC", 0))
+        + (0.3 * s.get("MP", 0) / 90)
+    )
