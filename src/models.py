026321ee439d239597306bfd634e455fe1c10afa
abowlingfan6 <asoccerfan6@gmail.com>
2026-06-21 15:07:23 -0700

    Refactor rating functions to improve calculations
    Updated rating calculations for defender, midfielder, and forward functions to use 'get' instead of 'normalized' for certain parameters.

--- src/models.py
+++ src/models.py
@@ -19,12 +19,12 @@ def normalized(row, col):
 def defender_rating(row):
     rating = 6.0
 
-    rating += 0.8 * normalized(row, "Tk")
-    rating += 0.8 * normalized(row, "INT")
+    rating += 0.1 * get(row, "Tk")
+    rating += 0.1 * get(row, "INT")
     rating += 0.4 * normalized(row, "P")
-    rating += 0.3 * normalized(row, "C")
-    rating += 0.3 * normalized(row, "FW")
-    rating += 0.3 * get(row, "PW")
+    rating += 0.15 * get(row, "C")
+    rating += 0.15 * get(row, "FW")
+    rating += 0.4 * get(row, "PW")
     rating += 0.75 * get(row, "A")
     rating += 1.5 * get(row, "G")
     rating += 0.2 * get(row, "SOnT")
@@ -42,17 +42,17 @@ def midfielder_rating(row):
     rating = 6.0
 
     rating += 0.7 * normalized(row, "P")
-    rating += 0.7 * normalized(row, "C")
-    rating += 0.5 * normalized(row, "Tk")
-    rating += 0.5 * normalized(row, "INT")
-    rating += 0.3 * normalized(row, "FW")
+    rating += 0.1 * get(row, "C")
+    rating += 0.05 * get(row, "Tk")
+    rating += 0.05 * get(row, "INT")
+    rating += 0.2 * get(row, "FW")
 
     rating += 0.6 * get(row, "A")
     rating += 1.25 * get(row,"G")
     rating += 0.4 * get(row, "SOnT")
     rating += 0.1 * get (row, "SOffT")
     rating += 0.3 * get(row, "BS")
-    rating += 0.3 * get(row, "PW")
+    rating += 0.4 * get(row, "PW")
 
     rating -= 0.15 * get(row, "FC")
     rating -= 0.3 * get(row, "YC")
@@ -71,13 +71,13 @@ def forward_rating(row):
 
     rating += 1.0 * get(row, "G")
     rating += 0.75 * get(row, "A")
-    rating += 0.7 * normalized(row, "SOnT")
-    rating += 0.3 * normalized(row, "SOffT")
-    rating += 0.4 * normalized(row, "BS")
-    rating += 0.3 * normalized(row, "FW")
+    rating += 0.3 * get(row, "SOnT")
+    rating += 0.1 * get(row, "SOffT")
+    rating += 0.1 * get(row, "BS")
+    rating += 0.1 * get(row, "FW")
     rating += 0.2 * normalized(row, "P")
 
-    rating -= 0.1 * get(row, "FC")
+    rating -= 0.2 * get(row, "FC")
     rating -= 0.1 * get(row, "O")
     rating -= 0.3 * get(row, "YC")
     rating -= 1.0 * get(row, "RC")
@@ -94,14 +94,14 @@ def sub_rating(row):
 
     rating += 0.9 * get(row, "G")
     rating += 0.5 * get(row, "A")
-    rating += 0.5 * normalized(row, "SOnT")
-    rating += 0.2 * normalized(row, "SOffT")
-    rating += 0.3 * normalized(row, "Tk")
-    rating += 0.3 * normalized(row, "INT")
+    rating += 0.3 * get(row, "SOnT")
+    rating += 0.1 * get(row, "SOffT")
+    rating += 0.075 * get(row, "Tk")
+    rating += 0.1 * get(row, "INT")
     rating += 0.2 * normalized(row, "P")
 
     rating -= 0.2 * get(row, "YC")
-    rating -= 0.8 * get(row, "RC")
+    rating -= 1.0 * get(row, "RC")
     rating -= 0.1 * get(row, "GC")
 
     minutes = get(row, "MP")
